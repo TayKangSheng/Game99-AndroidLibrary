b68d0ff1bd4ad64f8b1012a17f4992b8b56f71c6
TayKangSheng <kangsheng@gmail.com>
2015-03-13 19:47:15 +0800

    extra button classes

---
+++ README.md
@@ -0,0 +1,21 @@
+#  Game 42
+
+This android game is build by a group of 4.<br>
+To our fellow earth beings we might appear to be building this for a university CS course called Elements of Software Construction, 
+but under the cover we are <p>`answering the ultimate question of life, the universe and everything.`</p>
+
+which is easy and fun and even relaxing. The hard part is asking the right questions.
+
+<strong>Things to note:</strong><br>
+the game will only work with wi-fi connections (unless you are a hyper-intelligent, pan-dimentional being
+who emits wifi signals).
+<br><br>
+<strong>Developper:</strong><br>
+if you are getting server connection error, 
+try:<br>
+1. adding the <code>*.jar</code> files to your android library.<br>
+2. adding <code>\<uses-permission android:name="android.permission.INTERNET"\></code> to your <code>AndroidManifest.xml</code>.
+
+Don't panic<br>
+and oh,<br>
+<em>bring a towel.</em>
---
+++ src/com/init/Game99_AndroidLibrary/SocketIO.java
@@ -0,0 +1,103 @@
+package com.init.Game99_AndroidLibrary;
+
+import java.net.URISyntaxException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+public class SocketIO {
+	private static Socket socket;
+	private String ip = "http://128.199.225.246:8080";
+	public Socket getSocket(){
+		if(socket==null){
+			try {
+				socket = IO.socket("http://128.199.225.246:8080");
+				socket.connect(); // initiate connection to socket server
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			return socket;
+		}
+		return socket;
+	}
+	public SocketIO() {
+		if(socket==null){
+			try {
+				socket = IO.socket(ip);
+				socket.connect(); // initiate connection to socket server
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+	
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				Log.d("socketio", "socket connected");
+				socket.emit("joined", "I am android, and I just landed!");
+				// socket.disconnect(); // why is there a disconnect here?
+			}        
+		});
+		socket.on("ready", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					Assets.id = data.getInt("id");
+					Assets.msg = data.getString("msg");
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				
+				// socket.disconnect(); // why is there a disconnect here?
+			}        
+		});
+		socket.on("button", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				Assets.otherPlayerPress = (Integer) args[0];
+				// socket.disconnect(); // why is there a disconnect here?
+			}        
+		});
+		socket.on("gameover", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				Assets.gameover = true;
+				
+			}        
+		});
+		socket.on("grid", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				// TODO Auto-generated method stub
+				Log.d("socketio", "socket event connect error");
+				//there is a grid!
+				JSONObject data = (JSONObject) args[0];
+				try {
+					boolean[] tmp = new boolean[35];
+					JSONArray array = data.getJSONArray("grid");
+					if(Assets.id==0){
+						for(int i=0;i<35;i++) 
+							tmp[i] = array.getBoolean(i);
+					} else{
+						for(int i=0;i<35;i++) 
+							tmp[i] = !array.getBoolean(i);
+					}
+					Assets.interGalaticaMapVector = tmp;
+					Assets.ready = true;
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
