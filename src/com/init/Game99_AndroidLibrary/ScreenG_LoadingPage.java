16ca462c225dd670d810cc28337e79a120a91a92
Anusha <anusha@mymail.sutd.edu.sg>
2015-03-31 15:57:00 +0800

    31-3-2015-anusha

--- src/com/init/Game99_AndroidLibrary/ScreenG_LoadingPage.java
+++ src/com/init/Game99_AndroidLibrary/ScreenG_LoadingPage.java
@@ -13,6 +13,7 @@ import com.init.framework.Screen;
 public class ScreenG_LoadingPage extends Screen {
 	Paint load = new Paint();
 	String loading;
+	int gameHeight = game.getGraphics().getHeight();
 	//This is the loading page
 	public ScreenG_LoadingPage(Game game) {
 		super(game);
@@ -23,16 +24,36 @@ public class ScreenG_LoadingPage extends Screen {
 		// TODO Auto-generated method stub
 		Graphics graph = game.getGraphics();
 		graph.clearScreen(Color.LTGRAY);
-		painter.setColor(Color.BLACK);
-		painter.setTextSize(300);
-		graph.drawString("LOADING...",80 , gameHeight/2, painter);
+		load.setColor(Color.BLACK);
+		load.setTextSize(300);
+		graph.drawString("LOADING...",80 , gameHeight/2, load);
 		
 	}
 	@Override
 	public void update(float deltaTime) {
 		// TODO Auto-generated method stub
 		Assets.runTime += deltaTime;
-		game.setScreen(new ScreenD_GameScreen(game));
+		game.setScreen(new Screen_Initialize(game));
+		
+	}
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void backButton() {
+		// TODO Auto-generated method stub
 		
 	}
 	
