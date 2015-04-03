5d4f3f1da835e873791af8983d19a502815419c0
zikhan <zikanghan@gmail.com>
2015-04-03 12:07:56 +0800

    fixed bombed function in ButtonHandler

--- src/com/init/Game99_AndroidLibrary/Objects_ButtonHandler.java
+++ src/com/init/Game99_AndroidLibrary/Objects_ButtonHandler.java
@@ -55,96 +55,97 @@ public class Objects_ButtonHandler {
 				int[] r = round[8];
 				for(int i=0;i<r.length;i++) grid.get(index+r[i]).setNormalClickable();
 			}
-		}
-		
-		//if clicked on the right one
-		if (grid.get(index).getNormalClickable()){ 
-			if(grid.get(index).getInt() == small){
-				Assets.socketIO.getSocket().emit("button", index);
+		}else {
+			//if clicked on the right one
+			if (grid.get(index).getNormalClickable()){ 
+				if(grid.get(index).getInt() == small){
+					Assets.socketIO.getSocket().emit("button", index);
+					grid.get(index).setNormalNotClickable();
+					grid.get(index).shrink(7, 7);
+				}else{
+					grid.get(index).shake(14);
+					v.vibrate(500);
+				}
+			}
+			else if (grid.get(index).getBomb()){
+				Assets.socketIO.getSocket().emit("bomb", index);
+				if (index<=4 && index>=0){
+					if (index == 0){
+						int[] r = round[0];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					} else if (index == 4){
+						int[] r = round[1];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					} else{
+						int[] r = round[2];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					}
+				} else if (index<=34 && index>=30){
+					if (index == 30){
+						int[] r = round[3];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					} else if (index == 34){
+						int[] r = round[4];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					} else{
+						int[] r = round[5];
+						for(int i=0;i<r.length;i++){
+							grid.get(index+r[i]).setNormalNotClickable();
+						}
+					}			
+				} else if ( (index==5) || (index==10) || (index==15) || (index==20) || (index==25) ){
+					int[] r = round[6];
+					for(int i=0;i<r.length;i++){
+						grid.get(index+r[i]).setNormalNotClickable();
+					}
+				} else if ( (index==9) || (index==14) || (index==19) || (index==24) || (index==29) ){
+					int[] r = round[7];
+					for(int i=0;i<r.length;i++){
+						grid.get(index+r[i]).setNormalNotClickable();
+					}
+				} else{
+					int[] r = round[8];
+					for(int i=0;i<r.length;i++){
+						grid.get(index+r[i]).setNormalNotClickable();
+					}
+				}
+				
+			} else if (grid.get(index).getSmallest()){
 				grid.get(index).setNormalNotClickable();
-				grid.get(index).shrink(7, 7);
-			}else{
+				//if(grid.get(index).getlastclickable()) 
+					Assets.socketIO.getSocket().emit("button", index);
+				int tmp = 0;
+				ArrayList array = new ArrayList();
+				for(Objects_GridButton btn: grid){
+					if(btn.getInt()==small){
+						btn.setNormalNotClickable();
+						array.add(tmp);
+					}tmp ++;
+				}
+				Assets.socketIO.getSocket().emit("smallest", array);
+			} else if(grid.get(index).getHint()){
+				grid.get(index).setNormalNotClickable();
+				//if(grid.get(index).getlastclickable()) 
+					Assets.socketIO.getSocket().emit("button", index);
+			}
+			else{
 				grid.get(index).shake(14);
 				v.vibrate(500);
 			}
+		
 		}
-		else if (grid.get(index).getBomb()){
-			Assets.socketIO.getSocket().emit("bomb", index);
-			if (index<=4 && index>=0){
-				if (index == 0){
-					int[] r = round[0];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				} else if (index == 4){
-					int[] r = round[1];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				} else{
-					int[] r = round[2];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				}
-			} else if (index<=34 && index>=30){
-				if (index == 30){
-					int[] r = round[3];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				} else if (index == 34){
-					int[] r = round[4];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				} else{
-					int[] r = round[5];
-					for(int i=0;i<r.length;i++){
-						grid.get(index+r[i]).setNormalNotClickable();
-					}
-				}			
-			} else if ( (index==5) || (index==10) || (index==15) || (index==20) || (index==25) ){
-				int[] r = round[6];
-				for(int i=0;i<r.length;i++){
-					grid.get(index+r[i]).setNormalNotClickable();
-				}
-			} else if ( (index==9) || (index==14) || (index==19) || (index==24) || (index==29) ){
-				int[] r = round[7];
-				for(int i=0;i<r.length;i++){
-					grid.get(index+r[i]).setNormalNotClickable();
-				}
-			} else{
-				int[] r = round[8];
-				for(int i=0;i<r.length;i++){
-					grid.get(index+r[i]).setNormalNotClickable();
-				}
-			}
-			
-		} else if (grid.get(index).getSmallest()){
-			grid.get(index).setNormalNotClickable();
-			//if(grid.get(index).getlastclickable()) 
-				Assets.socketIO.getSocket().emit("button", index);
-			int tmp = 0;
-			ArrayList array = new ArrayList();
-			for(Objects_GridButton btn: grid){
-				if(btn.getInt()==small){
-					btn.setNormalNotClickable();
-					array.add(tmp);
-				}tmp ++;
-			}
-			Assets.socketIO.getSocket().emit("smallest", array);
-		} else if(grid.get(index).getHint()){
-			grid.get(index).setNormalNotClickable();
-			//if(grid.get(index).getlastclickable()) 
-				Assets.socketIO.getSocket().emit("button", index);
-		}
-		else{
-			grid.get(index).shake(14);
-			v.vibrate(500);
-		}
-
-	}}
+	}
+	}
 
 
 
